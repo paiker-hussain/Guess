4a65f54704efad61eb11311625ae2bd29d8df5a8
Paiker Hussain <97170032+paiker-hussain@users.noreply.github.com>
2023-05-28 16:39:31 +0500

    Update test_game.py

--- Test/test_game.py
+++ Test/test_game.py
@@ -2,36 +2,34 @@ from game import Game
 
 def test_initial_blank_word():
     game = Game("word")
-    assert game.get_current_word() == "____"
-
-def test_correct_guess():
-    game = Game("word")
-    assert game.make_guess("w") == "w___"
-
-def test_incorrect_guess():
-    game = Game("word")
-    assert game.make_guess("x") == "____"
+    assert game.current_word == "____"
 
 def test_multiple_correct_guesses():
     game = Game("intensity")
     assert game.make_guess("i") == "i_______"
-    assert game.make_guess("n") == "in______"
-    assert game.make_guess("t") == "int_____"
-    assert game.make_guess("e") == "inte____"
-    assert game.make_guess("s") == "inten___"
-    assert game.make_guess("i") == "inteni__"
-    assert game.make_guess("t") == "intenit_"
-    assert game.make_guess("y") == "intenity"
 
 def test_exhaust_attempts():
     game = Game("word")
-    assert game.make_guess("a") == "__"
-    assert game.make_guess("b") == "__"
-    assert game.make_guess("c") == "__"
-    assert game.make_guess("d") == "__"
-    assert game.make_guess("e") == "__"
-    assert game.make_guess("f") == "__"
-    assert game.make_guess("g") == "__"
-    assert game.make_guess("h") == "__"
-    assert game.make_guess("i") == "__"
-    assert game.make_guess("j") == "Game over!"
+    game.make_guess("a")
+    assert game.make_guess("a") == "____"
+
+def test_successful_guess():
+    game = Game("python")
+    assert game.make_guess("p") == "p____"
+    assert game.make_guess("y") == "py___"
+    assert game.make_guess("t") == "pyt__"
+    assert game.make_guess("h") == "pyth_"
+    assert game.make_guess("o") == "pytho"
+    assert game.make_guess("n") == "python"
+    assert game.make_guess("x") == "Congratulations! You guessed the word correctly."
+
+def test_hint():
+    game = Game("example")
+    hint = game.get_hint()
+    assert len(hint) == 35
+    assert hint.startswith("The hint is ")
+    assert hint.endswith(".")
+    assert hint[12] == "'"
+    assert hint[15] == "'"
+    assert hint[19] == "'"
+    assert hint[22] == "'"
