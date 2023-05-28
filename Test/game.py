b92de85933225a51ef791c3ec21122c868ee8293
Paiker Hussain <97170032+paiker-hussain@users.noreply.github.com>
2023-05-28 16:28:23 +0500

    Update game.py

--- Test/game.py
+++ Test/game.py
@@ -8,27 +8,29 @@ class Game:
         return self.current_word
 
     def make_guess(self, letter):
-        if letter in self.guesses:
-            return self.current_word
-
-        self.guesses.append(letter)
-
-        if letter in self.word:
-            self.update_current_word(letter)
-
-        if self.current_word == self.word:
-            return "Congratulations! You guessed the word correctly."
-
-        if len(self.guesses) >= 10:
-            return "Game over! You have used all your attempts."
-
+    if letter in self.guesses:
         return self.current_word
 
+    self.guesses.append(letter)
+
+    if letter in self.word:
+        self.update_current_word(letter)
+
+    if len(self.guesses) >= 10:
+        return "Game over! You have used all your attempts."
+
+    if self.current_word == self.word:
+        return "Congratulations! You guessed the word correctly."
+
+    return self.current_word
+
+
     def update_current_word(self, letter):
-        updated_word = ""
-        for i in range(len(self.word)):
-            if self.word[i] == letter:
-                updated_word += letter
-            else:
-                updated_word += self.current_word[i]
-        self.current_word = updated_word
+    updated_word = ""
+    for i in range(len(self.word)):
+        if self.word[i] == letter or self.current_word[i] != "_":
+            updated_word += self.word[i]
+        else:
+            updated_word += self.current_word[i]
+    self.current_word = updated_word
+
